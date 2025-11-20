384c653bc13883a43bd4ba7dd347f77642c11a06
mykola <gray.grifon@gmail.com>
2025-11-20 19:52:29 +0100

    add test and shadow

--- front/src/components/ui/containers/TextInput.tsx
+++ front/src/components/ui/containers/TextInput.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx"
-import { useRef, useState, type ChangeEvent } from "react"
+import { useRef, useState, type ChangeEvent, useId } from "react"
 
 type TInputError = {
     isError: boolean,
@@ -19,15 +19,23 @@ type TTextInput = TInput<string, string>
 const TextInput = (params: TTextInput) => {
 
     const [isFocus, setOnFocus] = useState(false);
+    const input = useRef(null);
+    const id = useId();
 
     const { onChange, placeholder = "", styles = "", value = '', isError = false, errorMessoge = "" } = params;
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => onChange(e.target.value);
     return (
         <div className="relative flex flex-col">
-            <label htmlFor="id" className={clsx("absolute bg-[var(--bg)]", isError && "text-[var(--error)]", value || isFocus ? "-top-3 left scale-70" : "top-2 left-2 opacity-90")}>{placeholder}</label>
+            <label htmlFor={id} className={clsx("absolute bg-[var(--bg)]", isError && "text-[var(--error)]", value || isFocus ? "-top-3 left scale-70" : "top-2 left-2 opacity-90")}>
+                <span className="opacity-70">
+
+                    {placeholder}
+                </span>
+            </label>
             <input
-                id="id"
+                ref={input}
+                id={id}
                 className={clsx(styles)}
                 type="text"
                 placeholder={placeholder}
@@ -43,5 +51,3 @@ const TextInput = (params: TTextInput) => {
 }
 
 export default TextInput
-
-//-top-3 left-2
